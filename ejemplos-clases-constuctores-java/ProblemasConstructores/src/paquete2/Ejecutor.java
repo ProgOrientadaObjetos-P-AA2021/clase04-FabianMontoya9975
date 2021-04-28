ff7fc622733de186ea2361c6f4d38ac3c489fa66
Fabián Montoya <jfmontoya1@utpl.edu.ec>
2021-04-28 17:15:23 -0500

    Resolución problema constructores paquete 2

--- ejemplos-clases-constuctores-java/ProblemasConstructores/src/paquete2/Ejecutor.java
+++ ejemplos-clases-constuctores-java/ProblemasConstructores/src/paquete2/Ejecutor.java
@@ -5,17 +5,33 @@
  */
 package paquete2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Ejecutor {
+
     public static void main(String[] args) {
-        
+        Scanner entrada = new Scanner(System.in);
         /*Crear un objeto y presentar los datos
         Ingresar la información por teclado.
-        */
-        
-        
+         */
+        String name;
+        String city;
+        int nDoc;
+        int nEnf;
+        System.out.println("Ingresar el nombre del hospital: ");
+        name = entrada.nextLine();
+        System.out.println("Ingresar la ciudad donde se ubica el hospital: ");
+        city = entrada.nextLine();
+        System.out.println("Ingresar el número de doctores que laboran: ");
+        nDoc = entrada.nextInt();
+        System.out.println("Ingresar el número de enfermeros que laboran:");
+        nEnf = entrada.nextInt();
+        Hospital hosp = new Hospital(name, city, nDoc, nEnf);
+
+        System.out.printf("%s\n", hosp);
     }
 }
--- ejemplos-clases-constuctores-java/ProblemasConstructores/src/paquete2/Hospital.java
+++ ejemplos-clases-constuctores-java/ProblemasConstructores/src/paquete2/Hospital.java
@@ -10,47 +10,67 @@ package paquete2;
  * @author reroes
  */
 public class Hospital {
+
     private String nombre;
     private String ciudad;
     private int numeroDoctores;
     private int numeroEnfermeros;
-    
+
     /*
         Crear un constructor que reciba como parámetros
         valores que serán asignados a los atributos.
     
-    */
-    
-    public void establecerNombre(String n){
+     */
+    public Hospital(String n, String c, int numD, int numE) {
+        nombre = n;
+        ciudad = c;
+        numeroDoctores = numD;
+        numeroEnfermeros = numE;
+    }
+
+    public void establecerNombre(String n) {
         nombre = n;
     }
-    
-    public void establecerCiudad(String n){
+
+    public void establecerCiudad(String n) {
         ciudad = n;
     }
-    
-    public void establecerNumeroDoctores(int n){
+
+    public void establecerNumeroDoctores(int n) {
         numeroDoctores = n;
     }
-    
-    public void establecerNumeroEnfermeros(int n){
+
+    public void establecerNumeroEnfermeros(int n) {
         numeroEnfermeros = n;
     }
-    
-    public String obtenerNombre(){
+
+    public String obtenerNombre() {
         return nombre;
     }
-    
-    public String obtenerCiudad(){
+
+    public String obtenerCiudad() {
         return ciudad;
     }
-    
-    public int obtenerNumeroDoctores(){
+
+    public int obtenerNumeroDoctores() {
         return numeroDoctores;
     }
-    
-    public int obtenerNumeroEnfermeros(){
+
+    public int obtenerNumeroEnfermeros() {
         return numeroEnfermeros;
     }
-    
+
+    @Override
+    public String toString() {
+        String cadena = String.format("\nNombre del hospital: %s\n"
+                + "Ciudad donde se ubica: %s\n"
+                + "Número de doctores: %d\n"
+                + "Número de enfermeros: %d\n",
+                obtenerNombre(),
+                obtenerCiudad(),
+                obtenerNumeroDoctores(),
+                obtenerNumeroEnfermeros());
+        return cadena;
+    }
+
 }
